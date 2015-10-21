adcd09188295fba6ff2851955e45a04c066748b8
Aleksandr <aleks.petushkov@gmail.com>
2015-10-21 22:15:38 +0600

    +

--- buh/traction.h
+++ buh/traction.h
@@ -11,32 +11,31 @@ class transaction
 	string note;
 
 public:
-	
-	//*
-	void Parse(string in) //
+	transaction(){}
+	transaction(string in)
 	{
-
-
 		size_t pos, pos1;
 		pos1 = in.find_first_of(" \t", pos);
-		date = in.substr(pos, pos1-pos);
+		date = in.substr(pos, pos1 - pos);
 
 		pos = in.find_first_not_of(" \t", pos1);
 		pos1 = in.find_first_of(" \t", pos);
-		source = in.substr(pos, pos1-pos);
-		
+		source = in.substr(pos, pos1 - pos);
+
 		pos = in.find_first_not_of(" \t", pos1);
 		pos1 = in.find_first_of(" \t", pos);
-		destination = in.substr(pos, pos1-pos);
-		
-		
+		destination = in.substr(pos, pos1 - pos);
+
+
 		pos = in.find_first_not_of(" \t", pos1);
 		pos1 = in.find_first_of(" \t", pos);
 		summ = atof((in.substr(pos, pos1 - pos)).c_str()); // Почему- то не работает перевод из строки в float, нужно спросить.
 
 		pos = in.find_first_not_of(" \t", pos1);
-		note = in.substr(pos, 1000); 
+		note = in.substr(pos, 1000);
 	}
+	
+
 	string getSource()
 	{
 		return source;
@@ -49,6 +48,11 @@ public:
 	double getSumm()
 	{
 		return summ;
+	}
+
+	string getDate()
+	{
+		return date;
 	}
 	//*/
 };
