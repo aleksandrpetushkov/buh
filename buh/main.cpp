d8339ac0301578aa829c130d60bc567541c93e3c
Aleksandr Petushkov <aleks.petushkov@gmail.com>
2015-10-23 11:38:19 +0600

    del catch

--- buh/main.cpp
+++ buh/main.cpp
@@ -7,300 +7,292 @@
 
 	using namespace std;
 
-
 	void main()
 	{
-		try
+		setlocale(LC_ALL, "ru-RU");
+		string tmp;
+		vector<transaction> massTransact; // Массив транзакций
+		vector<string> massSchetr; // Массив счетов
+		vector<string> massData; //Массив дат транзакций
+		fstream fs("file.txt", ios::in);
+		if (fs)
 		{
-			setlocale(LC_ALL, "ru-RU");
-			string tmp;
-			vector<transaction> massTransact; // Массив транзакций
-			vector<string> massSchetr; // Массив счетов
-			vector<string> massData; //Массив дат транзакций
-			fstream fs("file.txt", ios::in);
-			if (fs)
-			{
 
-				for (unsigned int k = 0; !fs.eof(); ++k)
+			for (unsigned int k = 0; !fs.eof(); ++k)
+			{
+				getline(fs, tmp);
+				if ((tmp[4] != tmp[7]) || (tmp[7] != '.'))
 				{
-					getline(fs, tmp);
-					if ((tmp[4] != tmp[7]) || (tmp[7] != '.'))
+					cerr << "Error: uncorrected string (" << tmp << ").\n";
+					cout << tmp[4] << " " << tmp[7] << endl;
+				}
+				else
+				{
+					massTransact.push_back(*new transaction(tmp));
+					if (massSchetr.size() == 0 && massTransact.size() == 1)
 					{
-						cerr << "Error: uncorrected string (" << tmp << ").\n";
-						cout << tmp[4] << " " << tmp[7] << endl;
+						massSchetr.push_back(massTransact[0].getSource());
+						if (massTransact[0].getSource() != massTransact[0].getDestinon())
+						{
+							massTransact[0].getDestinon();
+						}
 					}
 					else
 					{
-						massTransact.push_back(*new transaction(tmp));
-						if (massSchetr.size() == 0 && massTransact.size() == 1)
+						bool schetSource = false;
+						bool schetDestination = false;
+						for (unsigned int i = 0; i < massSchetr.size(); ++i)
 						{
-							massSchetr.push_back(massTransact[0].getSource());
-							if (massTransact[0].getSource() != massTransact[0].getDestinon())
+							if (massTransact[massTransact.size() - 1].getSource() == massSchetr[i])
 							{
-								massTransact[0].getDestinon();
+								schetSource = true;
 							}
+							if (massTransact[massTransact.size() - 1].getDestinon() == massSchetr[i])
+							{
+								schetDestination = true;
+							}
+						}
+						if (!schetSource)
+						{
+							massSchetr.push_back(massTransact[massTransact.size() - 1].getSource());
+						}
+						if (!schetDestination)
+						{
+							massSchetr.push_back(massTransact[massTransact.size() - 1].getDestinon());
+						}
+
+						if (massData.size() == 0 && massTransact.size() == 1)
+						{
+							massData.push_back(massTransact[0].getDate());
 						}
 						else
 						{
-							bool schetSource = false;
-							bool schetDestination = false;
-							for (unsigned int i = 0; i < massSchetr.size(); ++i)
+							bool dataNotExist = true;
+							for (unsigned int i = 0; i < massData.size(); ++i)
 							{
-								if (massTransact[massTransact.size() - 1].getSource() == massSchetr[i])
+								if (massData[massData.size() - 1] == massTransact[massTransact.size() - 1].getDate())
 								{
-									schetSource = true;
-								}
-								if (massTransact[massTransact.size() - 1].getDestinon() == massSchetr[i])
-								{
-									schetDestination = true;
+									dataNotExist = false;
+									break;
 								}
 							}
-							if (!schetSource)
+							if (dataNotExist)
 							{
-								massSchetr.push_back(massTransact[massTransact.size() - 1].getSource());
+								massData.push_back(massTransact[massTransact.size() - 1].getDate());
 							}
-							if (!schetDestination)
-							{
-								massSchetr.push_back(massTransact[massTransact.size() - 1].getDestinon());
-							}
+						}
 
-							if (massData.size() == 0 && massTransact.size() == 1)
+					}
+
+				}
+			}
+
+			fs.close();
+			unsigned int y = 0;
+			while (y != 9)
+			{
+
+				cout << "Enter number menu:\n1. Out all account.\n" << "2. Report time.\n9. Out program.\n";
+				while (true)
+				{
+					cin >> y;
+					if (cin.fail())
+					{
+						cin.sync();
+						cin.clear();
+						cin.ignore(cin.rdbuf()->in_avail());
+						cerr << "Error: incorrect enter, reenter: ";
+					}
+					else
+					{
+						break;
+					}
+				}
+				switch (y)
+				{
+				case 1:
+					for (unsigned int i = 0; i < massSchetr.size(); ++i)
+					{
+						double sm = 0;
+						for (unsigned int j = 0; j<massTransact.size(); ++j)
+						{
+							if (massSchetr[i] == massTransact[j].getSource())
 							{
-								massData.push_back(massTransact[0].getDate());
+								sm -= massTransact[j].getSumm();
+							}
+							else if (massSchetr[i] == massTransact[j].getDestinon())
+							{
+								sm += massTransact[j].getSumm();
+							}
+						}
+						cout << massSchetr[i] << " = " << sm << endl;
+					}
+					break;
+
+				case 2:
+					if (true)
+					{
+						unsigned int numberSchet = 0;
+						unsigned int numberDataBegin = 0;
+						unsigned int numberDateEnd = 0;
+						double begin = 0;
+						double in = 0;
+						double out = 0;
+						double Prihod = 0;
+						double Rashod = 0;
+						cout << "------------------------------------------------------------\n";
+						for (unsigned int i = 0; i < massSchetr.size(); ++i)
+						{
+							cout << i + 1 << ". " << massSchetr[i] << endl;
+
+						}
+						cout << "Enter number schet (no more " << massSchetr.size() << "): ";
+						while (true)
+						{
+							while (true)
+							{
+								cin >> numberSchet;
+								if (cin.fail())
+								{
+									cin.sync();
+									cin.clear();
+									cin.ignore(cin.rdbuf()->in_avail());
+									cerr << "Error: incorrect enter, reenter: ";
+								}
+								else
+								{
+									break;
+								}
+							}
+							if (numberSchet<1 || numberSchet>massSchetr.size())
+							{
+								cerr << "Error: incorrect number, reenter: ";
 							}
 							else
 							{
-								bool dataNotExist = true;
-								for (unsigned int i = 0; i < massData.size(); ++i)
-								{
-									if (massData[massData.size() - 1] == massTransact[massTransact.size() - 1].getDate())
-									{
-										dataNotExist = false;
-										break;
-									}
-								}
-								if (dataNotExist)
-								{
-									massData.push_back(massTransact[massTransact.size() - 1].getDate());
-								}
+								break;
 							}
 
 						}
-
-					}
-				}
-
-				fs.close();
-				unsigned int y = 0;
-				while (y != 9)
-				{
-					
-					cout << "Enter number menu:\n1. Out all account.\n" << "2. Report time.\n9. Out program.\n";
-					while (true)
-					{
-						cin >> y;
-						if (cin.fail())
+						cout << "------------------------------------------------------------\n";
+						for (unsigned int i = 0; i < massData.size(); ++i)
 						{
-							cin.sync();
-							cin.clear();
-							cin.ignore(cin.rdbuf()->in_avail());
-							cerr<< "Error: incorrect enter, reenter: ";
+							cout << i + 1 << ". " << massData[i] << endl;
 						}
-						else
-						{
-							break;
-						}
-					}
-					switch (y)
-					{
-					case 1:
-						for (unsigned int i = 0; i < massSchetr.size(); ++i)
-						{
-							double sm = 0;
-							for (unsigned int j = 0; j<massTransact.size(); ++j)
-							{
-								if (massSchetr[i] == massTransact[j].getSource())
-								{
-									sm -= massTransact[j].getSumm();
-								}
-								else if (massSchetr[i] == massTransact[j].getDestinon())
-								{
-									sm += massTransact[j].getSumm();
-								}
-							}
-							cout << massSchetr[i] << " = " << sm << endl;
-						}
-						break;
+						cout << "Enter number date begin: ";
 
-					case 2:
-						if (true)
+						while (true)
 						{
-							unsigned int numberSchet = 0;
-							unsigned int numberDataBegin = 0;
-							unsigned int numberDateEnd = 0;
-							double begin = 0;
-							double in = 0;
-							double out = 0;
-							double Prihod = 0;
-							double Rashod = 0;
-							cout << "------------------------------------------------------------\n";
-							for (unsigned int i = 0; i < massSchetr.size(); ++i)
-							{
-								cout << i + 1 << ". " << massSchetr[i] << endl;
-
-							}
-							cout << "Enter number schet (no more " << massSchetr.size() << "): ";
 							while (true)
 							{
-								while (true)
+								cin >> numberDataBegin;
+								if (cin.fail())
 								{
-									cin >> numberSchet;
-									if (cin.fail())
-									{
-										cin.sync();
-										cin.clear();
-										cin.ignore(cin.rdbuf()->in_avail());
-										cerr << "Error: incorrect enter, reenter: ";
-									}
-									else
-									{
-										break;
-									}
-								}
-								if (numberSchet<1 || numberSchet>massSchetr.size())
-								{
-									cerr << "Error: incorrect number, reenter: ";
-								}
-								else
-								{
-									break;
-								}
-								
-							}
-							cout << "------------------------------------------------------------\n";
-							for (unsigned int i = 0; i < massData.size(); ++i)
-							{
-								cout << i + 1 << ". " << massData[i] << endl;
-							}
-							cout << "Enter number date begin: ";
-
-							while (true)
-							{
-								while (true)
-								{
-									cin >> numberDataBegin;
-									if (cin.fail())
-									{
-										cin.sync();
-										cin.clear();
-										cin.ignore(cin.rdbuf()->in_avail());
-										cerr << "Error: incorrect enter, reenter: ";
-									}
-									else
-									{
-										break;
-									}
-								}
-								if (numberDataBegin<1 || numberDataBegin>massData.size())
-								{
-									cerr << "Error: incorrect number, reenter: ";
+									cin.sync();
+									cin.clear();
+									cin.ignore(cin.rdbuf()->in_avail());
+									cerr << "Error: incorrect enter, reenter: ";
 								}
 								else
 								{
 									break;
 								}
 							}
-							cout << "------------------------------------------------------------\n";
-							for (unsigned int i = numberDataBegin - 1; i < massData.size(); ++i)
+							if (numberDataBegin<1 || numberDataBegin>massData.size())
 							{
-								cout << i + 1 << ". " << massData[i] << endl;
+								cerr << "Error: incorrect number, reenter: ";
 							}
-							cout << "Enter number date end: ";
+							else
+							{
+								break;
+							}
+						}
+						cout << "------------------------------------------------------------\n";
+						for (unsigned int i = numberDataBegin - 1; i < massData.size(); ++i)
+						{
+							cout << i + 1 << ". " << massData[i] << endl;
+						}
+						cout << "Enter number date end: ";
 
+						while (true)
+						{
 							while (true)
 							{
-								while (true)
+								cin >> numberDateEnd;
+								if (cin.fail())
 								{
-									cin >> numberDateEnd;
-									if (cin.fail())
-									{
-										cin.sync();
-										cin.clear();
-										cin.ignore(cin.rdbuf()->in_avail());
-										cerr << "Error: incorrect enter, reenter: ";
-									}
-									else
-									{
-										break;
-									}
-								}
-								if (numberDateEnd<numberDataBegin || numberDateEnd>massData.size())
-								{
-									cerr << "Error: incorrect number, reenter: ";
+									cin.sync();
+									cin.clear();
+									cin.ignore(cin.rdbuf()->in_avail());
+									cerr << "Error: incorrect enter, reenter: ";
 								}
 								else
 								{
 									break;
 								}
 							}
-							// Счетаем баланс по транзакциям на начало периода
-							unsigned int i = 0;
-							for (; i != massTransact.size(); ++i)
+							if (numberDateEnd<numberDataBegin || numberDateEnd>massData.size())
 							{
-								if (massData[numberDataBegin-1] == massTransact[i].getDate()) 
-								{
-									break;
-								}
-								if (massSchetr[numberSchet - 1] == massTransact[i].getSource())
-								{
-									in -= massTransact[i].getSumm();
-								}
-								else if (massSchetr[numberSchet - 1] == massTransact[i].getDestinon())
-								{
-									in += massTransact[i].getSumm();
-								}
-
+								cerr << "Error: incorrect number, reenter: ";
 							}
-							// Счетем расход/приход за пераод
-							for (; i != massTransact.size(); ++i)
+							else
 							{
-								if (massSchetr[numberSchet - 1] == massTransact[i].getSource()) // Если движение денег происходит со счета это расход
-								{
-									Rashod += massTransact[i].getSumm();
-								}
-								else if (massSchetr[numberSchet - 1] == massTransact[i].getDestinon()) // Если движение денег происходит на счет это приход
-								{
-									Prihod += massTransact[i].getSumm();
-								}
-								if (massData[numberDateEnd - 1] == massTransact[i].getDate()) 
-								{
-									break;
-								}
+								break;
 							}
-							cout << "Begin: " << in << endl;
-							cout << "Rashod: " << Rashod << endl;
-							cout << "Prihod: " << Prihod << endl;
-							cout << "Result: " << in - Rashod + Prihod << endl;
 						}
-						break;
-					case 9:
-						break;
+						// Счетаем баланс по транзакциям на начало периода
+						unsigned int i = 0;
+						for (; i != massTransact.size(); ++i)
+						{
+							if (massData[numberDataBegin - 1] == massTransact[i].getDate())
+							{
+								break;
+							}
+							if (massSchetr[numberSchet - 1] == massTransact[i].getSource())
+							{
+								in -= massTransact[i].getSumm();
+							}
+							else if (massSchetr[numberSchet - 1] == massTransact[i].getDestinon())
+							{
+								in += massTransact[i].getSumm();
+							}
 
-					default:
-						cout << y << " - Not exist\n";
-						break;
+						}
+						// Счетем расход/приход за пераод
+						for (; i != massTransact.size(); ++i)
+						{
+							if (massSchetr[numberSchet - 1] == massTransact[i].getSource()) // Если движение денег происходит со счета это расход
+							{
+								Rashod += massTransact[i].getSumm();
+							}
+							else if (massSchetr[numberSchet - 1] == massTransact[i].getDestinon()) // Если движение денег происходит на счет это приход
+							{
+								Prihod += massTransact[i].getSumm();
+							}
+							if (massData[numberDateEnd - 1] == massTransact[i].getDate())
+							{
+								break;
+							}
+						}
+						cout << "Begin: " << in << endl;
+						cout << "Rashod: " << Rashod << endl;
+						cout << "Prihod: " << Prihod << endl;
+						cout << "Result: " << in - Rashod + Prihod << endl;
 					}
+					break;
+				case 9:
+					break;
+
+				default:
+					cout << y << " - Not exist\n";
+					break;
 				}
 			}
-			else
-			{
-				cerr << "Error: cannot open file" << endl;
-			}
-
-			system("pause");
 		}
-		catch (const std::exception& ex)
+		else
 		{
-			cerr << "(((";
+			cerr << "Error: cannot open file" << endl;
 		}
+
+		system("pause");
 	}
